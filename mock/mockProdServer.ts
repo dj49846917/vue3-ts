52c3388471f81440586335461ed081c8660bcfdc
dj49846917 <821084785@qq.com>
2021-04-09 17:55:51 +0800

    接口

--- mock/mockProdServer.ts
+++ mock/mockProdServer.ts
@@ -1,8 +1,9 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
 import auction from './auction';
 import user from './login';
-import common from './common'
+import common from './common';
+import personal from './personal';
 
 export function setupProdMockServer() {
-  createProdMockServer([...auction, ...user, ...common]);
+  createProdMockServer([...auction, ...user, ...common, ...personal]);
 }
