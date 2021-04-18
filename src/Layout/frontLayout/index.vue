aff8c65790aae0a3568b471b390a70ff07c15ad3
dj49846917 <821084785@qq.com>
2021-04-18 10:36:24 +0800

    登录退出

--- src/Layout/frontLayout/index.vue
+++ src/Layout/frontLayout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <front-header />
+  <front-header :isLogin="isLogin" :userInfo="userInfo" />
   <front-search />
   <front-nav v-if="!isLogin" />
   <router-view />
@@ -7,12 +7,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import FrontHeader from "@/components/Header.vue";
 import FrontSearch from "@/Layout/frontLayout/Search.vue";
 import FrontNav from "@/Layout/frontLayout/Nav.vue";
 import FrontFooter from "@/components/Footer.vue";
 import { onBeforeRouteUpdate, useRoute } from "vue-router";
+import { UserInfo } from "@/types/types";
+import Cookies from "js-cookie";
+import { CookieConfig } from "@/constant/config";
 export default defineComponent({
   components: {
     FrontHeader,
@@ -23,11 +26,24 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const isLogin = ref(false)
+    let userInfo: UserInfo = reactive({
+      DealerInfo: null,
+      LoginUserInfo: {},
+    });
     if(route.path === "/login" || route.path === "/register") {
       isLogin.value = true
     } else {
       isLogin.value = false
     }
+
+    try {
+      if(Cookies.get(CookieConfig.USER_INFO)) {
+        userInfo = JSON.parse(Cookies.get(CookieConfig.USER_INFO) as string);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+
 
     // 监听路由值得变化
     onBeforeRouteUpdate((to): void => {
@@ -40,6 +56,7 @@ export default defineComponent({
     });
     return {
       isLogin,
+      userInfo
     };
   },
 });
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,21 +4,44 @@
       <p class="fl gray9 time">早上好，欢迎来到荣投地产！</p>
       <p class="fr">
         <span class="yellow tell">服务热线：023-63080760</span>&emsp;&emsp;
-        <a href="#"><span>立即登录</span></a>
-        &emsp;| &emsp;
-        <a href="#"><span>免费注册</span></a>
+        <span v-if="!isLogin">
+          <span>{{ userInfo.LoginUserInfo.NickName }}</span>
+          &emsp;| &emsp;
+          <span @click="closeApp()" class="offBtn">退出</span>
+        </span>
+        <span v-else>
+          <a href="#"><span>立即登录</span></a>
+          &emsp;| &emsp;
+          <a href="#"><span>免费注册</span></a>
+        </span>
       </p>
     </div>
   </header>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Cookies from "js-cookie";
+import { CookieConfig } from '@/constant/config';
+import router from '@/router';
+export default defineComponent({
+  props: ['isLogin', 'userInfo'],
   setup() {
-    return {};
+    // 退出
+    const closeApp = () => {
+      Cookies.remove(CookieConfig.USER_INFO)
+      router.push("/login")
+    }
+    
+    return {
+      closeApp
+    };
   },
-};
+});
 </script>
 
 <style lang="scss" scoped>
+.offBtn {
+  cursor: pointer;
+}
 </style>
